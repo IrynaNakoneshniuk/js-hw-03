07c256a3c32179ab21bbc05c81d76e5843d66328
IrynaNakoneshniuk <103773315+IrynaNakoneshniuk@users.noreply.github.com>
2023-04-23 00:18:09 +0300

    first commit

---
+++ js/04.js
@@ -0,0 +1,30 @@
+// Напиши функцію countTotalSalary(employees) приймаючу об'єкт зарплат. Функція рахує загальну суму зарплати працівників і повертає її. Кожне поле об'єкта, переданого в функцію, має вигляд "ім'я":"зарплата".
+
+const countTotalSalary = function (employees) {
+    let sumSalary = 0;
+    if(employees){
+        for (let key in employees) {
+          sumSalary += employees[key];
+        }
+    }
+    return sumSalary;
+};
+
+
+console.log(countTotalSalary({})); // 0
+
+console.log(
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80,
+  }),
+); // 330
+
+console.log(
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150,
+  }),
+); // 400
