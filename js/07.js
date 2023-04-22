b21c169cee8e72af660b8b9969a0d3930d03058c
IrynaNakoneshniuk <103773315+IrynaNakoneshniuk@users.noreply.github.com>
2023-04-23 00:38:31 +0300

    add 7 task

---
+++ js/07.js
@@ -0,0 +1,95 @@
+// Напиши сценарій керування особистим кабінетом інтернет-банку. Є об'єкт account в якому необхідно реалізувати методи для роботи з балансом та історією транзакцій.
+
+const Transaction = {
+  DEPOSIT: 'deposit',
+  WITHDRAW: 'withdraw',
+};
+
+/*
+ * Кожна транзакція - це об'єкт з властивостями: id, type і amount
+ */
+
+const account = {
+  // Поточний баланс рахунку
+  balance: 0,
+
+  // Історія транзакцій
+  transactions: [],
+
+  /*
+   * Метод створює і повертає об'єкт транзакції.
+   * Приймає суму і тип транзакції.
+   */
+  generateId(){
+    return this.transactions.length+1;
+  },
+
+  createTransaction(amount, type) {
+
+    const newTransaction = {
+      id: this.generateId(),
+      type,
+      amount,
+    };
+    return newTransaction;
+  },
+
+  /*
+   * Метод відповідає за додавання суми до балансу.
+   * Приймає суму танзакції.
+   * Викликає createTransaction для створення об'єкта транзакції
+   * після чого додає його в історію транзакцій
+   */
+  deposit(amount) {
+    this.balance += amount;
+    const newTransaction = this.createTransaction(amount, Transaction.DEPOSIT);
+    this.transactions.push(newTransaction);
+  },
+
+  /*
+   * Метод відповідає за зняття суми з балансу.
+   * Приймає суму танзакції.
+   * Викликає createTransaction для створення об'єкта транзакції
+   * після чого додає його в історію транзакцій.
+   *
+   * Якщо amount більше, ніж поточний баланс, виводь повідомлення
+   * про те, що зняття такої суми не можливо, недостатньо коштів.
+   */
+  withdraw(amount) {
+    if (this.balance >= amount) {
+      this.balance -= amount;
+      const newTransaction = this.createTransaction(amount, Transaction.WITHDRAW);
+      this.transactions.push(newTransaction);
+    } else {
+      return 'Недостатньо коштів';
+    }
+  },
+
+  /*
+   * Метод повертає поточний баланс
+   */
+  getBalance() {
+    return this.balance;
+  },
+
+  /*
+   * Метод шукає і повертає об'єкт транзакції по id
+   */
+  getTransactionDetails(id) {
+    return this.transactions.find((transaction) => transaction.id === id);
+  },
+
+  /*
+   * Метод повертає кількість коштів
+   * певного типу транзакції з усієї історії транзакцій
+   */
+  getTransactionTotal(type) {
+    let total = 0;
+    this.transactions.forEach((transaction) => {
+      if (transaction.type === type) {
+        total += transaction.amount;
+      }
+    });
+    return total;
+  },
+};
